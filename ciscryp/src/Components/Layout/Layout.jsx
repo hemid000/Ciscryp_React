5722250c814f8280f442cf8605dd1b1287460e60
hemid000 <hmid.yusifli04@mail.ru>
2023-01-19 13:43:45 +0400

    husky added

---
+++ ciscryp/src/Components/Layout/Layout.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Navbar from "./Navbar/Navbar";
+import Footer from "./Footer/Footer";
+import { createContext, useState } from "react";
+import "../Layout/Layout.scss";
+export const ThemeContext = createContext(null);
+const Layout = ({ children }) => {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div id={theme}>
+        <>
+          <Navbar />
+          {children}
+          <Footer />
+        </>
+      </div>
+    </ThemeContext.Provider>
+  );
+};
+
+export default Layout;
---
+++ ciscryp/src/Components/Layout/Navbar/Navbar.jsx
@@ -0,0 +1,58 @@
+import "../Navbar/Nav.scss";
+import React from "react";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import Button from "react-bootstrap/Button";
+import { AiOutlineSearch } from "react-icons/ai";
+import { AiOutlineDown } from "react-icons/ai";
+import ReactSwitch from "react-switch";
+const Navbar = () => {
+  return (
+    <nav id="nav">
+      <div className="container mt-3">
+        <div className="row">
+          <div className="col-5">
+            <div className="left_Side">
+              <img
+                id="home_img"
+                src="https://chisnghiax.com/ciscryp/static/media/logo.25debea968a8cdc57717f9110ced774d.svg"
+                alt=""
+              />
+
+              <InputGroup className="mb-3">
+                <Form.Control
+                  aria-label="Default"
+                  aria-describedby="inputGroup-sizing-default"
+                  placeholder="Search item"
+                />
+                <Button variant="outline-secondary" id="button-addon1">
+                  <AiOutlineSearch />
+                </Button>
+              </InputGroup>
+            </div>
+          </div>
+          <div className="col-7">
+            <div className="right_side">
+              <ul>
+                <li id="discover">
+                  Discover <AiOutlineDown />
+                </li>
+                <li>Help center</li>
+              </ul>
+              <ReactSwitch></ReactSwitch>
+              <ul id="create">
+                <li>Create</li>
+              </ul>
+              <img
+                src="https://chisnghiax.com/ciscryp/static/media/Image-8.5ae85a64aab1965e33a5.png"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
